481761d373752e44b710f4cede1b195533ef399d
drin <vllasidrin@gmail.com>
2026-04-25 11:19:39 +0200

    ok

--- mobileApp/components/ProfileInfo.js
+++ mobileApp/components/ProfileInfo.js
@@ -1,55 +1,98 @@
-import React from "react";  
-import {Text, Image, View, StyleSheet} from "react-native";
+import React from "react";
+import { Text, Image, View, StyleSheet, Button, TouchableOpacity } from "react-native";
 
-const ProfileInfo = (props) => {
-    return(
-        <View style={styles.container}>
-            <View style={styles.cardWrapper}>
-                <View style={styles.imgWrapper}>
-                    <Image source={props.image} style={styles.img}/>
-                </View>
-                <View style={styles.infoWrapper}>
-                    <Text style={styles.fullname}>{props.fullname}</Text>
-                    <Text style={styles.position}>{props.position}</Text>
-                    <Text>{props.description}</Text>
-                </View>
-            </View>
+const ProfileInfo = ({ image, fullname, position, description, projects }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cardWrapper}>
+        <View style={styles.imgWrapper}>
+          <Image
+            source={image}
+            style={styles.img}
+            resizeMode="cover"
+          />
         </View>
-        
-    )
-}
+
+        <View style={styles.infoWrapper}>
+          <Text style={styles.fullname}>{fullname}</Text>
+          <Text style={styles.position}>{position}</Text>
+          <Text style={styles.description}>{description}</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Hire Him</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection:"column",
-    },
-    cardWrapper: {
-        flexDirection: 'column',
-        backgroundColor: 'white',
-        borderBottomRightRadius: 8,
-        borderBottomLeftRadius: 8,
-        alignSelf: 'center',
-    },
-    img:{
-        width: 100,
-        height: 100,
-        alignSelf: 'center',
-    },
-    infoWrapper: {
-        marginLeft: 20,
-        marginRight: 20,
-        flexWrap: 'wrap',
-        marginTop: 20,
-        alignSelf: 'center',
-    },
-    fullname:{
-        fontWeight: 'bold',
-        alignSelf: 'center',
-    },
-    position: {
-        fontWeight: 'bold',
-        alignSelf: 'center',
-    }
+  container: {
+    width: "30%",
+    alignSelf: "center",
+    
+  },
+  cardWrapper: {
+    backgroundColor: "white",
+    alignSelf: "center",
+    overflow: "hidden",
+    height: "200%"
+  },
+  imgWrapper: {
+    backgroundColor: "lightblue",
+    width: "100%",
+    height: "31%",
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center"
+  },
+  img: {
+    width: "70%",
+    height: 335,
+    marginTop: 80,
+  },
+  infoWrapper: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "grey",
+    marginHorizontal: 20,
+    position: "static",
+    marginTop: -50,
+    width: "80%",
+    height: "20%",
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  fullname: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  position: {
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#FFD700", // nice gold/yellow
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 10,
+    marginBottom: 10
+  },
+
+  buttonText: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center",
+  }
 });
 
 export default ProfileInfo;
--- mobileApp/screens/ProfileScreen.js
+++ mobileApp/screens/ProfileScreen.js
@@ -1,24 +1,102 @@
 import React from "react";
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Image,
+  SafeAreaView,
+} from "react-native";
 import ProfileInfo from "../components/ProfileInfo";
 
-
 const ProfileScreen = () => {
-    return (
-        <View>
-            <ProfileInfo fullname="Drin Vllasi" image={require("../assets/avatarimg.png")} position="Full Stack Web Developer" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standar"/>
-        </View>
-    );
+  const projects = [
+    { name: "Project1", img: require("../assets/project1.jpg") },
+    { name: "Project2", img: require("../assets/project2.png") },
+    { name: "Project3", img: require("../assets/project3.png") },
+    { name: "Project4", img: require("../assets/project4.png") },
+    { name: "Project5", img: require("../assets/project5.png") },
+    { name: "Project6", img: require("../assets/project6.jpg") },
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={styles.card}>
+      <Image source={item.img} style={styles.projectImage} />
+      <Text style={styles.projectItem}>{item.name}</Text>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.screen}>
+      <FlatList
+        data={projects}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+        showsVerticalScrollIndicator={true}
+        contentContainerStyle={styles.listContainer}
+        columnWrapperStyle={styles.row}
+        ListHeaderComponent={
+          <>
+            <ProfileInfo
+              fullname="Drin Vllasi"
+              image={require("../assets/avatarimg.png")}
+              position="Full Stack Web Developer"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+              button="Hire Him"
+              projects={projects}
+            />
+
+            <Text style={styles.projectsText}>Projects</Text>
+          </>
+        }
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
+  );
 };
 
-
 const styles = StyleSheet.create({
-    text: {
-        textAlign: 'center',
-        fontSize: 20,
-        marginVertical: 20
-    }
-});
+  screen: {
+    flex: 1, // 🔥 CRITICAL
+  },
 
+  listContainer: {
+    paddingBottom: 30,
+  },
+
+  projectsText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 15,
+    textAlign: "center",
+  },
+
+  row: {
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    marginBottom: 15,
+  },
+
+  card: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 12,
+    alignItems: "center",
+    width: "48%",
+    elevation: 3,
+  },
+
+  projectImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
+    marginBottom: 8,
+  },
+
+  projectItem: {
+    textAlign: "center",
+    fontSize: 14,
+  },
+});
 
 export default ProfileScreen;
